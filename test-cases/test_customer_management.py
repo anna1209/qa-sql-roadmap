9901ec480eff58d58716bdebd6ac27897a9fe77c
Anna Wang <annawang9712@gmail.com>
2026-07-22 15:40:12 -0400

    attempt fix run tests issue

--- test-cases/test_customer_management.py
+++ test-cases/test_customer_management.py
@@ -4,7 +4,7 @@ import mysql.connector
 def get_connection():
     return mysql.connector.connect(**db_connection)
 
-def verify_customers_have_addresses():
+def test_verify_customers_have_addresses():
     """
     TC-CM-003:
     Verify every customer has at least one address in the addresses table.
